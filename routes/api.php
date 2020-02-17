b69209806a1c83d62d5c50b85c26b756069767a5
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-17 11:02:54 -0400

    middleware routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    /*AÑADE AQUI LAS RUTAS QUE QUIERAS PROTEGER CON JWT*/
+});
