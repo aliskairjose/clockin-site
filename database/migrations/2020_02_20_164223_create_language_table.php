bbf9843039a427303cc6bed5c7900f72203cb863
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-20 12:50:21 -0400

    achivos

---
+++ database/migrations/2020_02_20_164223_create_language_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('locale',10)->unique();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language');
+    }
+
+}
