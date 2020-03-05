aba5d470201a7aedd02d06679503e08ad4180314
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-03-05 14:56:54 -0400

    ajus

--- app/Http/Controllers/ClockRecordingController.php
+++ app/Http/Controllers/ClockRecordingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ClockRecording extends Controller
+class ClockRecordingController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class ClockRecording extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.schedule.index');
     }
 
     /**
